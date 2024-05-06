6ffd2a9b1c037e0d985f379da8d90cfe0bfb29c4
Tatiana Tarazona <leydy.tarazona@gmail.com>
2024-05-05 23:18:58 -0500

    Se agregan rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Residentes',
+    children: [
+      {
+        path: 'home',
+        title: 'Residentes - Inicio',
+        component: HomeComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
