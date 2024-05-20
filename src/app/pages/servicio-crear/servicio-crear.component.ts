2cff27c56ebedcc8582d90dfba030210164aae6a
Tatiana Tarazona <leydy.tarazona@gmail.com>
2024-05-19 22:14:37 -0500

    Agregar pagina de servicios y crear servicio

--- src/app/pages/servicio-crear/servicio-crear.component.ts
+++ src/app/pages/servicio-crear/servicio-crear.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-servicio-crear',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './servicio-crear.component.html',
-  styleUrl: './servicio-crear.component.css'
+  styleUrl: './servicio-crear.component.css',
 })
-export class ServicioCrearComponent {
+export class ServicioCrearComponent implements OnInit {
+  crearServicioForm: FormGroup;
+  private formBuilder = inject(FormBuilder);
 
+  ngOnInit(): void {
+    this.crearServicioForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      descripcion: ['', [Validators.required]],
+      apertura: ['', [Validators.required]],
+      cierre: ['', [Validators.required]],
+      dias: ['', [Validators.required]],
+      encargado: ['', [Validators.required]],
+      estado: ['', [Validators.required]],
+    });
+  }
+
+  crearServicio() {
+    console.log(this.crearServicioForm.value);
+  }
 }
--- src/app/pages/servicio-lista/servicio-lista.component.ts
+++ src/app/pages/servicio-lista/servicio-lista.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { TableComponent } from '../../shared/table/table.component';
 
 @Component({
   selector: 'app-servicio-lista',
   standalone: true,
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './servicio-lista.component.html',
-  styleUrl: './servicio-lista.component.css'
+  styleUrl: './servicio-lista.component.css',
 })
 export class ServicioListaComponent {
+  router = inject(Router);
 
+  public listado = [
+    {
+      nombre: 'Piscina',
+      descripcion: 'Piscina olimpica para adultos',
+      apertura: '4:00 pm',
+      cierre: '8:00 pm',
+      dias: 'lunes, miercoles, jueves',
+      encargado: 'Pepito',
+      estado: 'Habilitado',
+    },
+    {
+      nombre: 'Gimnasio',
+      descripcion: 'Ginasio semidotado',
+      apertura: '4:00 pm',
+      cierre: '8:00 pm',
+      dias: 'lunes, martes, jueves',
+      encargado: 'Juanito',
+      estado: 'Habilitado',
+    },
+  ];
+
+  redirigirCrearServicio() {
+    this.router.navigateByUrl('servicio-crear');
+  }
 }
