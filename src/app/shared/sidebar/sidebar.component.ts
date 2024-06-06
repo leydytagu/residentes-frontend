76d3d5d3dd6049237107fc9fc2937f34e9b5b4e1
Tatiana Tarazona <leydy.tarazona@gmail.com>
2024-06-06 19:08:56 -0400

    Se agrega boton para cerrar sesion

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MenuRoutes } from '../../core/enum/menu.enum';
 import { MenuInterface } from '../../core/interfaces/menu.interface';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ResidentesService } from '../../services/residentes.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,10 +12,15 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent implements OnInit {
+  private residentesService = inject(ResidentesService);
   menuItems: MenuInterface[] = [];
 
   ngOnInit(): void {
     this.menuItems = MenuRoutes;
     console.log(this.menuItems);
+  }
+
+  cerrarSesion() {
+    this.residentesService.logout();
   }
 }
