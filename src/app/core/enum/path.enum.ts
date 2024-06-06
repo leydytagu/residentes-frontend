2dfcc17b3c28680073c5d4464f4527c5d5c75fa5
Tatiana Tarazona <leydy.tarazona@gmail.com>
2024-06-06 19:08:33 -0400

    Enum de rutas

---
+++ src/app/core/enum/path.enum.ts
@@ -0,0 +1,13 @@
+export enum PATH {
+  HOME = 'home',
+  RESIDENTE_LISTA = 'residente-lista',
+  RESIDENTE_CREAR = 'residente-crear',
+  RESIDENTE_DETALLE = 'residente-detalle',
+  SERVICIO_LISTA = 'servicio-lista',
+  SERVICIO_CREAR = 'servicio-crear',
+  SERVICIO_DETALLE = 'servicio-detalle',
+  RESERVA_LISTA = 'reserva-lista',
+  RESERVA_CREAR = 'reserva-crear',
+  RESERVA_DETALLE = 'reserva-detalle',
+  LOGIN = 'login',
+}
