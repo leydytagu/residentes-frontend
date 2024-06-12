0e3a259bcf58b542a803881411ddd060b5a5ec01
Tatiana Tarazona <leydy.tarazona@gmail.com>
2024-06-12 17:03:07 -0500

    Se agrega pantalla de home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ResidentesService } from '../../services/residentes.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  nombreUsuario: string;
 
+  private residentesService = inject(ResidentesService);
+
+  ngOnInit() {
+    this.nombreUsuario = this.residentesService.usuario?.nombre;
+  }
 }
